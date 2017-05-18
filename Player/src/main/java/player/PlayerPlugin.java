3bbc7414570c074689495a24db05cb7e1cda46d3
Mads Mosumgaard <mads1108@hotmail.dk>
2017-05-18 02:35:23 +0200

    AI

--- Player/src/main/java/player/PlayerPlugin.java
+++ Player/src/main/java/player/PlayerPlugin.java
@@ -75,7 +75,7 @@ public class PlayerPlugin implements IEntityProcessingService, IGamePluginServic
         
         Body body = new Body(50, 50, Geometry.RECTANGLE);
         player.add(body);
-        player.setMoveState(MovementState.STANDINGRIGHT);
+        player.setMoveState(MovementState.STANDINGDOWN);
         player.setCharState(CharacterState.IDLE);
         world.addEntity(player);
 
